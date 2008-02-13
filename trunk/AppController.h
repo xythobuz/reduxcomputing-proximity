9de46d7ddca26b65fd5a166726c061cc5792fa31
denver.timothy <denver.timothy@1f542621-0c46-0410-b387-c5b88493177f>
2008-02-13 16:23:35 +0000

    Initial commit.

---
+++ trunk/AppController.h
@@ -0,0 +1,27 @@
+/* AppController */
+
+#import <Cocoa/Cocoa.h>
+#import "BluetoothProximityController.h"
+
+
+@interface AppController : NSObject
+{
+    IBOutlet NSTextField *deviceStatus;
+    IBOutlet NSButton *enabled;
+    IBOutlet NSTextField *inRangePath;
+    IBOutlet NSTextField *outOfRangePath;
+    IBOutlet NSTextField *seconds;
+	IBOutlet NSProgressIndicator *progress;
+	
+	BluetoothProximityController *proximityController;
+}
+
+- (IBAction)changeDevice:(id)sender;
+- (IBAction)changeInRangePath:(id)sender;
+- (IBAction)changeOutOfRangePath:(id)sender;
+- (IBAction)checkConnection:(id)sender;
+- (IBAction)enable:(id)sender;
+- (IBAction)testInRangeScript:(id)sender;
+- (IBAction)testOutOfRangeScript:(id)sender;
+
+@end
---
+++ trunk/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Proximity
+//
+//  Created by Denver Timothy on 1/8/07.
+//  Copyright My New Company Name 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
